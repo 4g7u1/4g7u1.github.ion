304b1a5170d7575b639af96812bf2e25b0e1f3a2
bigcat_li <101077453+4g7u1@users.noreply.github.com>
2022-04-25 19:01:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 export default {
   title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
+  nickName: "bigcat_li", // 昵称
+  githubName: '4g7u1', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
   domain: 'https://blog.halberd.cn', // rss域名
